0f58f6e6f39bd0737089075c841916e24c65fde5
Drew Sing <sing.drew@gmail.com>
2013-09-23 10:24:22 -0700

    redoing hangman for fun

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ hangman
 
 Play hangman in ruby! 
 
-runhangman.rb file used to run the game from terminal. twohangman.rb is not relevant. 
+
--- hangman.rb
+++ hangman.rb
@@ -1,94 +1,65 @@
 
+# require 'pry'
 
 class Hangman
-	LIST = ["dog"]
-  attr_accessor :word, :chances, :board, :list, :guesses
 
-  def initialize()
-    @chances = 8
-    @guesses = []
-    @word    = LIST.sample
-    @board   = setup_board(@word)
-  end
-  
-  # return @guesses as a string
-  def guesses
-  	return @guesses.join(", ")
-  end
-  
-  # return a string of underscores equal to length of the given word
-  def setup_board(word)
-  		boardstring = ""
-  		@word.length.times do 
-				boardstring << "_"
-			end
-		return boardstring.split("") # store the letters in board as an array
-	end
+  attr_accessor :turns_left
 
-  # return true if word has letter
-  def word_has?(letter)
-  	@word.index(letter) == nil ? false : true
-  end
+  def initialize
+    words_array = ["bulls", "ted"]
+    @word = words_array.sample        # is this the best place to put our picked word,
+                                    # or should it be in a method?
+    
+    @word_array = @word.split(//)
 
-  # replace indexes of @board with letter where the same indexes of @word are letter
-  # in other words, if @board is _ _ _ _ _ _ and @word is canada
-  # and the letter guessed is a
-  # then @board should become _ a _ a _ a
+    @game_board = []
+    
+    @word.length.times do |i|
+      @game_board << "_"
 
-  def put_letter_on_board(letter)
-    word_array = @word.split("")
-    word_array.each_with_index do |element, index|
-      if element == letter
-        @board[index] = element
-      end
+    @turns_left = 11
     end
   end
-
-  # decrement # of chances and add letter to guesses
-  def wrong_letter(letter)
-  	@chances -= 1
-  	@guesses << letter
+    
+  def game_board
+    puts
+    print @game_board
+    puts 
+    puts "turns left #{@turns_left}"
   end
-  
-  # if the word has the given letter, put it on the board, otherwise, it's a wrong guess
-  # ! Completed? But what about the case where the wrong letter has already been guessed.
 
   def guess(letter)
-    if @board.include?(letter)
-      puts "You've already guessed that letter! Try again!\n"
-
-    elsif letter.kind_of?(Fixnum)
-      puts "Please enter a valid letter" 
-      guess(letter)
-      
-      #trying to get it to display error is a number is typed
-
-    elsif word_has?(letter)
-    	put_letter_on_board(letter)
-    elsif @guesses.include? letter
-      puts "You've already guessed that letter! Try again!\n"
-    else wrong_letter(letter) 
+    if @word_array.include?(letter)
+      @word_array.each_with_index {|let,index|
+        if @word_array[index] == letter
+          @game_board[index] = letter
+        end
+        }
+    else turns_left
     end
   end
 
-  # return true if @board doesn't have a '_', otherwise return false
+  def turns_left
+    @turns_left -= 1
+  end
 
-  def win?
-    found_count = 0
-    @board.each do |letter|
-      if letter == '_'
-        found_count = found_count + 1
-      end
+  def winner?
+    if @game_board == @word_array
+      puts "YOU WINNNN!"
     end
-    found_count == 0 ? true : false
   end
-
-  # return true if @chances is 0, otherwise return false
-
-  def lost?
-      @chances == 0 # using a ternary operator to return true/false
-  end
-  
 end
 
+game = Hangman.new
+game.turns_left
+puts 
+p "Welcome to hangman! Enter a letter guess:"
 
+
+  until game.instance_variable_get(:@turns_left) > 0 || game.winner?
+    game.game_board
+    game.guess(gets.chomp)
+    
+    #get this to end when either turns_left expires or a game.winner is found?
+    
+  end
