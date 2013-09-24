c0989a2e78b4850e15a7215f1f73398d1a889891
Drew Sing <sing.drew@gmail.com>
2013-09-24 12:21:33 -0700

    added class for runner

--- hangman.rb
+++ hangman.rb
@@ -7,11 +7,11 @@ class Hangman
   def initialize
     words = ["bulls", "ted"]
     @word = words.sample        # is this the best place to put our picked word,
-                                    # or should it be in a method?    
+                                # or should it be in a method?    
     @turns_left = 10
   end
     
-  def display_board
+  def initial_board
     @game_board = []
     
     @word.length.times do |i|
@@ -24,6 +24,7 @@ class Hangman
 
   def show_board
     print @game_board
+    puts
   end
 
   def guess(letter)
@@ -46,40 +47,36 @@ class Hangman
     puts
   end
 
-  # def turns_left
-  #   @turns_left -= 1
-  # end
-
-  def space
-    puts 
-  end
-
   def winner
     if @game_board == @word_array
-      puts "YOU WINNNN!"
+      puts "YOU WIN!"
     elsif @turns_left == 0
-      puts "YOU LOSEEE"
+      puts "Sorry, you lose. the word was #{@word}"
+
     end
   end
 end
 
-game = Hangman.new
 
-puts 
 
-p "Welcome to hangman! Enter a letter guess:"
+class Runner
+  def self.run
+    @game = Hangman.new
+    puts 
+    p "Welcome to hangman! Enter a letter guess:"
 
-    game.display_board
+    @game.initial_board
 
-  while true
-    game.guess(gets.chomp)
-    game.display_turns
-    # game.turns_left
-    # game.winner 
-    game.show_board
-    game.space
-    game.winner
+    while true
+      @game.guess(gets.chomp)
+      @game.display_turns
+      @game.show_board
+      @game.winner
+    end
   end
+end
+
+Runner.run
 
   
   #QUESTIONS 
